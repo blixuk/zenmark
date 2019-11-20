c1a68e68526587f5d45db2d9d95fbd2c29c2d22b
blixuk <blixuk@gmail.com>
2019-11-20 18:17:19 +0000

    Formatting fixes

--- zenmark.py
+++ zenmark.py
@@ -23,7 +23,7 @@ class Tokenizer:
 		self.lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "
 
 		self.tokens = [] # List of all tokens created
-	
+
 	# Take a document and process it into segments and then pass it to the tokenizer
 	def lex(self, document, filename):
 		segments = list(document) # List out the document into segments
@@ -31,7 +31,7 @@ class Tokenizer:
 		# Loop through each segment in the list and pass it to the tokenizer
 		for segment in segments: 
 			self.tokenize(segment) # Pass segments to tokenizer
-		
+
 		#self.debug(self.tokens)
 		self.write(filename)
 	
@@ -41,29 +41,33 @@ class Tokenizer:
 		# Heading Standard
 		if re.findall(r'(HS)([1-6])(.*?)$', segment):
 			for t in re.findall(r'(HS)([1-6])(.*?)$', segment):
+				heading = self.headingSize(t[1])
 				segment = re.sub(r'(HS)([1-6])(.*?)$', 
-				self.tag('tspan', t[2].strip(), fontSize = self.headingSize(t[1]), x = self.paperMarginLeft, dy = self.fontSpacing), 
+				self.tag('tspan', t[2].strip(), fontSize = heading[0], x = self.paperMarginLeft, dy = heading[1]), 
 				segment)
 
 		# Heading Underlined
 		if re.findall(r'(HU)([1-6])(.*?)$', segment):
 			for t in re.findall(r'(HU)([1-6])(.*?)$', segment):
+				heading = self.headingSize(t[1])
 				segment = re.sub(r'(HU)([1-6])(.*?)$', 
-				self.tag('tspan', t[2].strip(), fontSize = self.headingSize(t[1]), x = self.paperMarginLeft, dy = self.fontSpacing, textDecoration = "underline"),
+				self.tag('tspan', t[2].strip(), fontSize = heading[0], x = self.paperMarginLeft, dy = heading[1], textDecoration = "underline"),
 				segment)
 		
 		# Title Standard
 		if re.findall(r'(TS)([1-6])(.*?)$', segment):
 			for t in re.findall(r'(TS)([1-6])(.*?)$', segment):
+				heading = self.headingSize(t[1])
 				segment = re.sub(r'(TS)([1-6])(.*?)$', 
-				self.tag('tspan', t[2].strip(), fontSize = self.headingSize(t[1]), x = "50%", dy = self.fontSpacing, textAnchor = "middle"), 
+				self.tag('tspan', t[2].strip(), fontSize = heading[0], x = "50%", dy = heading[1], textAnchor = "middle"), 
 				segment)
 
 		# Title Underlined
 		if re.findall(r'(TU)([1-6])(.*?)$', segment):
 			for t in re.findall(r'(TU)([1-6])(.*?)$', segment):
+				heading = self.headingSize(t[1])
 				segment = re.sub(r'(TU)([1-6])(.*?)$', 
-				self.tag('tspan', t[2].strip(), fontSize = self.headingSize(t[1]), x = "50%", dy = self.fontSpacing, textAnchor = "middle", textDecoration = "underline"), 
+				self.tag('tspan', t[2].strip(), fontSize = heading[0], x = "50%", dy = heading[1], textAnchor = "middle", textDecoration = "underline"), 
 				segment)
 
 		# Introduction
@@ -71,15 +75,16 @@ class Tokenizer:
 			for t in re.findall(r'(IN)([1-4])(.*?)$', segment):
 				if t[1] == "2":
 					segment = re.sub(r'(IN)([1-4])(.*?)$',
-					self.tag('tspan', t[2].strip(), fontSize = 18, x = "50%", dy = self.fontSpacing, textAnchor = "middle", textDecoration = "underline", fontStyle = "italic"), 
+					self.tag('tspan', t[2].strip(), fontSize = 20, x = "50%", dy = "1.6em", textAnchor = "middle", textDecoration = "underline", fontStyle = "italic"), 
 					segment)
 				if t[1] == "3":
 					segment = re.sub(r'(IN)([1-4])(.*?)$',
-					self.tag('tspan', t[2].strip(), fontSize = 14, x = "50%", dy = self.fontSpacing, textAnchor = "middle", fontStyle = "italic"), 
+					self.tag('tspan', t[2].strip(), fontSize = 15, x = "50%", dy = "1.8em", textAnchor = "middle", fontStyle = "italic"), 
 					segment)
 				else:
+					heading = self.headingSize(t[1])
 					segment = re.sub(r'(IN)([1-4])(.*?)$',
-					self.tag('tspan', t[2].strip(), fontSize = self.headingSize(t[1]), x = "50%", dy = self.fontSpacing, textAnchor = "middle"), 
+					self.tag('tspan', t[2].strip(), fontSize = heading[0], x = "50%", dy = heading[1], textAnchor = "middle"), 
 					segment)
 
 		# Paragraph Standard
@@ -104,7 +109,7 @@ class Tokenizer:
 
 				self.wordWrap(t[2], size, True)
 
-		#  Blank Space Fixed and Adjustable
+		# Blank Space Fixed and Adjustable
 		if re.findall(r'(SP)([0-9]*)', segment):
 			for t in re.findall(r'(SP)([0-9]*)', segment):
 				if t[1] == '':
@@ -113,6 +118,14 @@ class Tokenizer:
 					spacing = t[1]
 				
 				segment = re.sub(r'(SP)([0-9]*)', self.tag('tspan', self.blankSpace, dy = f'{spacing}em'), segment)
+
+		# Bold
+		if re.findall(r'\*\*(.*?)\*\*', segment):
+			for t in re.findall(r'\*\*(.*?)\*\*', segment):
+				print(t)
+				segment = re.sub(r'\*\*(.*?)\*\*', 
+				self.tag('tspan', t, fontWeight = "bold", space = "preserve"),
+				segment)
 
 		self.tokens.append(segment.strip('\n').strip('\t')) # Clean new line and tabs from tokens
 
@@ -125,25 +138,18 @@ class Tokenizer:
 	def tag(self, name, *content, 
 		fontSize = None,
 		fontStyle = None,
+		fontWeight = None,
 		textAnchor = None,
 		textDecoration = None,
 		space = None,
 		**attrs):
 
-		if fontSize is not None:
-			attrs['font-size'] = fontSize
-		
-		if fontStyle is not None:
-			attrs['font-style'] = fontStyle		
-
-		if textAnchor is not None:
-			attrs['text-anchor'] = textAnchor
-		
-		if textDecoration is not None:
-			attrs['text-decoration'] = textDecoration
-
-		if space is not None:
-			attrs['xml:space'] = space
+		if fontSize is not None: attrs['font-size'] = fontSize
+		if fontStyle is not None: attrs['font-style'] = fontStyle		
+		if fontWeight is not None: attrs['font-weight'] = fontWeight
+		if textAnchor is not None: attrs['text-anchor'] = textAnchor	
+		if textDecoration is not None: attrs['text-decoration'] = textDecoration
+		if space is not None: attrs['xml:space'] = space
 
 		if attrs:
 			attr_str = ''.join(f' {attr}="{value}"'
@@ -161,26 +167,29 @@ class Tokenizer:
 	# Heading Size Assignment
 	def headingSize(self, heading):
 		if heading == '1':
-			size = 36
+			size = 35
+			spacing = "1.5em"
 		elif heading == '2':
-			size = 24
+			size = 30
+			spacing = "1.2em"
 		elif heading == '3':
-			size = 18
+			size = 25
+			spacing = "1.4em"
 		elif heading == '4':
-			size = 14
+			size = 20
+			spacing = "1.6em"
 		elif heading == '5':
-			size = 10
+			size = 15
+			spacing = "1.8em"
 		elif heading == '6':
-			size = 8
+			size = 10
+			spacing = "2"
 		
-		return size
+		return [size, spacing]
 
 	# Word Wrap
 	def wordWrap(self, text, size, tab = False):
-		#wrapPoint = ((self.paperBodyWidth - len(text)) / 2) - ((int(size) + 4) * 2)
 		wrapPoint = (self.paperBodyWidth / (int(size) / 2.5) )
-		print(int(wrapPoint))
-		print('\n')
 		wrapper = textwrap.TextWrapper(width = int(wrapPoint))
 		if tab != False:
 			text = '\n\n\n\n\n\n\n\n' + text
@@ -192,7 +201,14 @@ class Tokenizer:
 			else:
 				spacing = self.fontSpacing
 			
-			self.tokens.append(self.tag('tspan', segment, fontSize = size, x = self.paperMarginLeft, dy = spacing, space = "preserve"))
+			if count > 1:
+				textLength = self.paperBodyWidth
+			else:
+				textLength = ""
+
+			#self.tokens.append(self.tag('tspan', segment, fontSize = size, x = self.paperMarginLeft, dy = spacing, space = "preserve", textLength = textLength))
+			token = self.tag('tspan', segment, fontSize = size, x = self.paperMarginLeft, dy = spacing, space = "preserve", textLength = textLength)
+			self.tokenize(token)
 			count -= 1
 
 	# Write Document
